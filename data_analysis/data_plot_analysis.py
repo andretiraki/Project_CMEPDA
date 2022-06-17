fd948b3dfde196ea1c009b58b204f890649b2b41
Manuel Luci <m.luci@studenti.unipi.it>
2022-06-17 02:01:03 +0200

    first module of data analysis

--- data_analysis/data_plot_analysis.py
+++ data_analysis/data_plot_analysis.py
@@ -13,51 +13,51 @@ if __name__ == '__main__':
     start = time.time()
     data = load_info_data(PATH)
     #Visualizing loan payoff and chargeoff
-    countpl(column = 'loan_status' , data = data, save = 1, name = '(1)pf_vs_co.pdf')
+    countpl(column = 'loan_status' , data = data,  save = 0, name = '(1)pf_vs_co.pdf')
     #Compute Matrix Correlation and plot it
-    corr_matrix(data, save = 1, name ='(2)corr_matrix.pdf')
+    corr_matrix(data,  save = 0, name ='(2)corr_matrix.pdf')
     #Visualize relationship between grade and loan status
     sorted_grade = sorted(data['grade'].unique())
-    countpl(column = 'grade', data = data, hue= 'loan_status', save = 1, \
+    countpl(column = 'grade', data = data, hue= 'loan_status',  save = 0, \
             name = '(3)grade.pdf', order = sorted_grade)
     #Grade is a good indicator if the borrower has the ability to payoff or not
     grade_co = data[data['loan_status'] == 'Charged Off'].groupby('grade').count()['loan_status']
     grade_fp = data[data['loan_status'] == 'Fully Paid'].groupby('grade').count()['loan_status']
-    barpl(grade_co, grade_fp, save = 1, name = '(4)grade_bar.pdf')
+    barpl(grade_co, grade_fp,  save = 0, name = '(4)grade_bar.pdf')
     #Visualize relationship between subgrade and loan status
     sorted_sub_grade = sorted(data['sub_grade'].unique())
-    countpl(column = 'sub_grade', data = data, hue= 'loan_status', save = 1, \
+    countpl(column = 'sub_grade', data = data, hue= 'loan_status',  save = 0, \
             name = '(5)sub_grade.pdf', order = sorted_sub_grade)
     sub_grade_co = data[data['loan_status'] == "Charged Off"].groupby("sub_grade").count()['loan_status']
     sub_grade_fp = data[data['loan_status'] == 'Fully Paid'].groupby('sub_grade').count()['loan_status']
     #They give more informations than grade
-    barpl(sub_grade_co, sub_grade_fp, save = 1, name = '(6)sub_grade_bar.pdf')
+    barpl(sub_grade_co, sub_grade_fp,  save = 0, name = '(6)sub_grade_bar.pdf')
     #Visualize relationship between  loan_status and verification_status
     sorted_verification_status = sorted(data["verification_status"].unique())
-    countpl(column = 'verification_status' , data = data, hue='loan_status', save = 1, \
+    countpl(column = 'verification_status' , data = data, hue='loan_status',  save = 0, \
             name = '(7)verification_status.pdf')
     #Source Verified and Verified have almost same information
     verification_status_co = data[data["loan_status"] == "Charged Off"].groupby("verification_status").count()["loan_status"]
     verification_status_fp = data[data["loan_status"] == "Fully Paid"].groupby("verification_status").count()["loan_status"]
-    barpl(verification_status_co, verification_status_fp, save = 1,\
+    barpl(verification_status_co, verification_status_fp,  save = 0,\
           name = '(8)verification_status_bar.pdf')
     #Visualize relationship between int_rate and loan_status
     sorted_int_rate = sorted(data['int_rate'].unique())
-    histpl(column = 'int_rate', data = data, save = 1, \
+    histpl(column = 'int_rate', data = data,  save = 0, \
            name = '(9)int_rate_hist.pdf', hue = 'loan_status', binwidth= 2)
     #Visualizie relationship betweeen emp_length and loan_status
     sorted(data["emp_length"].dropna().unique())
     sorted_emp_length = ['< 1 year', '1 year', '2 years', '3 years', '4 years',\
                          '5 years', '6 years', '7 years', '8 years', '9 years', '10+ years']
-    countpl(column = 'emp_length', data = data, hue= 'loan_status', save = 1, \
+    countpl(column = 'emp_length', data = data, hue= 'loan_status',  save = 0, \
             order=sorted_emp_length, name = '(10)emp_lenght.pdf')
     #emp_length doesn't help in ditinguish borrowers who payoff or not
     emp_co = data[data["loan_status"] == "Charged Off"].groupby("emp_length").count()["loan_status"]
     emp_fp = data[data["loan_status"] == "Fully Paid"].groupby("emp_length").count()["loan_status"]
-    barpl(emp_co, emp_fp, save = 1,\
+    barpl(emp_co, emp_fp,  save = 0,\
           name = '(11)emp_lenght_bar.pdf')
     #Visualize relationship between  purprose and loan_status
-    countpl(column = 'purpose', data = data, hue= 'loan_status', save = 1, \
+    countpl(column = 'purpose', data = data, hue= 'loan_status',  save = 0, \
             name = '(12)purpose.pdf')
     mins = (time.time()-start)//60
     sec = (time.time()-start) % 60
